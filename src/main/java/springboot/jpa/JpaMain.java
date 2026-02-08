ca91c864466e7f44bf1e81ff2f45e7ca55678357
bioroid17 <trace1997@ajou.ac.kr>
2026-02-08 23:12:40 +0900

    고급 매핑: 실전 예제 4 - 상속관계 매핑

--- src/main/java/springboot/jpa/JpaMain.java
+++ src/main/java/springboot/jpa/JpaMain.java
@@ -4,9 +4,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
-import springboot.jpa.domain.Member;
-import springboot.jpa.domain.Order;
-import springboot.jpa.domain.OrderItem;
+import springboot.jpa.domain.Book;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -17,8 +15,11 @@ public class JpaMain {
         tx.begin();
 
         try {
-           Order order = new Order();
-           order.addOrderItem(new OrderItem());
+            Book book = new Book();
+            book.setName("JPA");
+            book.setAuthor("김영한");
+
+            em.persist(book);
 
            tx.commit();
         } catch (Exception e) {
