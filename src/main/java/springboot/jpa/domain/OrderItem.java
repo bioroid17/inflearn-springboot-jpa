166e8c168fc639cfc6d50debe34066916e6d1693
bioroid17 <trace1997@ajou.ac.kr>
2026-02-18 04:21:21 +0900

    프록시와 연관관계 관리: 실전 예제 5 - 연관관계 관리

--- src/main/java/springboot/jpa/domain/OrderItem.java
+++ src/main/java/springboot/jpa/domain/OrderItem.java
@@ -10,17 +10,11 @@ public class OrderItem extends BaseEntity {
     @Column(name = "ORDER_ITEM_ID")
     private Long id;
 
-//    @Column(name="ORDER_ID")
-//    private Long orderId;
-//
-//    @Column(name="ITEM_ID")
-//    private Long itemId;
-
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "ORDER_ID")
     private Order order;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "ITEM_ID")
     private Item item;
 
