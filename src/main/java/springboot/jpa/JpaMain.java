c2aa62e6d3d0c289eee7a89de554fc0b94dc3492
bioroid17 <trace1997@ajou.ac.kr>
2026-02-25 02:15:33 +0900

    값 타입 - 값 타입과 불변 객체

--- src/main/java/springboot/jpa/JpaMain.java
+++ src/main/java/springboot/jpa/JpaMain.java
@@ -11,12 +11,14 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setUsername("hello");
-            member.setHomeAddress(new Address("city", "street", "10000"));
-            member.setWorkPeriod(new Period());
+            Address address = new Address("city", "street", "10000");
+            Member member1 = new Member();
+            member1.setUsername("member1");
+            member1.setHomeAddress(address);
+            em.persist(member1);
 
-            em.persist(member);
+            Address newAddress = new Address("NewCity", address.getStreet(), address.getZipcode());
+            member1.setHomeAddress(newAddress);
 
             tx.commit();
         } catch (Exception e) {
