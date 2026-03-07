85b847eb8d891a9aa32369719d54d79dabc76af0
bioroid17 <trace1997@ajou.ac.kr>
2026-03-07 13:58:25 +0900

    객체지향 쿼리 언어1 - 기본 문법: 소개

--- src/main/java/springboot/jpa/JpaMain.java
+++ src/main/java/springboot/jpa/JpaMain.java
@@ -1,6 +1,9 @@
 package springboot.jpa;
 
 import jakarta.persistence.*;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 
 import java.util.List;
 import java.util.Set;
@@ -16,33 +19,15 @@ public class JpaMain {
         try {
             Member member = new Member();
             member.setUsername("member1");
-            member.setHomeAddress(new Address("homeCity", "street1", "10000"));
-
-            member.getFavoriteFoods().add("치킨");
-            member.getFavoriteFoods().add("족발");
-            member.getFavoriteFoods().add("피자");
-
-            member.getAddressHistory().add(new AddressEntity("old1", "street1", "10000"));
-            member.getAddressHistory().add(new AddressEntity("old2", "street1", "10000"));
-
             em.persist(member);
 
-            em.flush();
-            em.clear();
+            // flush -> commit, query
 
-            System.out.println("============= START =============");
-            Member findMember = em.find(Member.class, member.getId());
+            List<Member> resultList = em.createNativeQuery("select MEMBER_ID, city, street, zipcode, USERNAME from MEMBER", Member.class).getResultList();
 
-//            findMember.getHomeAddress().setCity("newCity");
-//            Address a = findMember.getHomeAddress();
-//            findMember.setHomeAddress(new Address("newCity", a.getStreet(), a.getZipcode()));
-
-            // 치킨 -> 한식
-//            findMember.getFavoriteFoods().remove("치킨");
-//            findMember.getFavoriteFoods().add("한식");
-//
-//            findMember.getAddressHistory().remove(new Address("old1", "street1", "10000"));
-//            findMember.getAddressHistory().add(new Address("newCity1", "street1", "10000"));
+            for (Member member1 : resultList) {
+                System.out.println("member1: " + member1);
+            }
 
             tx.commit();
         } catch (Exception e) {
