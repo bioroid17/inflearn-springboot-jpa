3ec3a10ade7bcedc65cc68347912cfe0f73c55ed
bioroid17 <trace1997@ajou.ac.kr>
2026-03-20 01:43:43 +0900

    객체지향 쿼리 언어1 - 기본 문법: JPQL 함수

--- src/main/java/springboot/jpql/JpqlMain.java
+++ src/main/java/springboot/jpql/JpqlMain.java
@@ -13,23 +13,21 @@ public class JpqlMain {
         tx.begin();
 
         try {
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Member member1 = new Member();
+            member1.setUsername("관리자1");
+            em.persist(member1);
 
-            Member member = new Member();
-            member.setUsername("관리자");
-            member.setAge(10);
-            member.setType(MemberType.ADMIN);
-
-            member.setTeam(team);
-
-            em.persist(member);
+            Member member2 = new Member();
+            member2.setUsername("관리자2");
+            em.persist(member2);
 
             em.flush();
             em.clear();
 
-            String query = "select nullif(m.username, '관리자') from Member m ";
+//            String query = "select concat('a', 'b') from Member m";
+//            String query = "select locate('de', 'abcdefg') from Member m";
+//            String query = "select size(t.members) from Team t";
+            String query = "select group_concat(m.username) from Member m";
             List<String> result = em.createQuery(query, String.class)
                     .getResultList();
 
