dfc7e64220c97769dffc9462b7b58c941816febb
bioroid17 <trace1997@ajou.ac.kr>
2026-03-22 05:12:29 +0900

    객체지향 쿼리 언어2 - 중급 문법: 경로 표현식

--- src/main/java/springboot/jpql/JpqlMain.java
+++ src/main/java/springboot/jpql/JpqlMain.java
@@ -13,27 +13,28 @@ public class JpqlMain {
         tx.begin();
 
         try {
+            Team team = new Team();
+            em.persist(team);
+
             Member member1 = new Member();
             member1.setUsername("관리자1");
+            member1.setTeam(team);
             em.persist(member1);
 
             Member member2 = new Member();
             member2.setUsername("관리자2");
+            member2.setTeam(team);
             em.persist(member2);
 
             em.flush();
             em.clear();
 
-//            String query = "select concat('a', 'b') from Member m";
-//            String query = "select locate('de', 'abcdefg') from Member m";
-//            String query = "select size(t.members) from Team t";
-            String query = "select group_concat(m.username) from Member m";
+            String query = "select m.username from Team t join t.members m";
             List<String> result = em.createQuery(query, String.class)
                     .getResultList();
 
-            for (String s : result) {
-                System.out.println("s = " + s);
-            }
+            System.out.println("result: " + result);
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
